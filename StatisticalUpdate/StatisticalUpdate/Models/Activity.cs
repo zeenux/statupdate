676fac76f3ab5e5b2ae2dca0167bf90a4adcf9d0
Zeeshan Hayat <10649557+zeenux@users.noreply.github.com>
2018-04-28 09:53:05 +0500

    Activity.cs SMStatistics.cs

---
+++ StatisticalUpdate/StatisticalUpdate/Models/Activity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StatisticalUpdate.Models
+{
+    public class Activity
+    {
+        public int id { get; set; }
+        public String ActivityType { get; set; }
+    }
+}
---
+++ StatisticalUpdate/StatisticalUpdate/Models/SMStatistics.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StatisticalUpdate.Models
+{
+    public class SMStatistics
+    {
+        public int Id { get; set; }
+        public int CBOType { get; set; }
+        public DateTime PeriodFrom { get; set; }
+        public DateTime PeriodTo { get; set; }
+        public int ActivityType { get; set; }
+        public Double ActivityValue { get; set; }
+        public int DistrictID { get; set; }
+
+    }
+}
